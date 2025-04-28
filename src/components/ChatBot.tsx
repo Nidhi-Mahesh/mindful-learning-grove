d6377a33973f9344437f284a2cf9e8060229f8c2
Nidhi-Mahesh <nidhi.mahesh1010@gmail.com>
2025-04-28 17:20:32 +0530

    Update import2 ChatBot.tsx

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -1,4 +1,4 @@
-import { getBotResponse } from '../utils/chatresponses';
+import { getBotResponse } from '../../utils/chatResponse';
 import { useState } from 'react';
 import { MessageSquare, X } from 'lucide-react';
 import { cn } from '@/lib/utils';
